31061f9b97994b4a776e9f3222c2566f39f05f52
sRrAiN98 <89556955+sRrAiN98@users.noreply.github.com>
2023-06-10 18:31:02 +0900

    Revert "(Directory) 2023-06-08 문제 풀이"
    This reverts commit b59a41be92a98bd3a2bcf0a80f5e51217b31f7ec.

--- level1/약수의 합/README.md
+++
@@ -1,2 +0,0 @@
-# 약수의 합
-[약수의 합](https://school.programmers.co.kr/learn/courses/30/lessons/12928)
--- level1/약수의 합/solution.py
+++
@@ -1,8 +0,0 @@
-def solution(n):
-    answer = 0
-    strn = str(n)
-    print(strn)
-    for i in range(len(str(n))):
-        answer += int(strn[i])
-
-    return answer
